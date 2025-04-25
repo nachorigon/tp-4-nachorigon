8631790916e56db95ae78845d8e89dca45475c87
nachorigon <nacho.rigon05@gmail.com>
2025-04-25 12:24:51 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    year = int (input("Ingrese un año: "))
+    
+    if ((year % 100 != 0) and (year % 4 == 0)) or ((year % 100 == 0) and (year % 400 == 0))
+        print(f"El año {year} es bisiesto")
+    else:
+        print(f"El año {year} no es bisiesto")
+    
